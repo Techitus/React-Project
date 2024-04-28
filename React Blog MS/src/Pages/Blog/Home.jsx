5cd59660d54550270cbf60bbc2256dccb5b2070d
Bikram Gyawali <157723263+CoddingJourney@users.noreply.github.com>
2024-04-27 23:56:12 -0700

    Delete Blog Code
    In Single Blocg Delete blog and single page blog made

--- React Blog MS/src/Pages/Blog/Home.jsx
+++ React Blog MS/src/Pages/Blog/Home.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../Components/Layout/Layout";
 import Card from "./components/Card/Card";
+import axios, { Axios } from "axios";
+import { baseUrl } from "../../config";
 
 const Home = () => {
+  const [blogs, setBlog] = useState([]);
+  const fetchBlogs = async () => {
+    const response = await axios.get(`${baseUrl}/blog`);
+    if (response.status === 200) {
+      setBlog(response.data.data);
+    }
+  };
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
   return (
     <Layout>
-      <div className="flex flex-wrap justify-center ">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+      <div className="flex flex-wrap justify-center  ">
+        {blogs.length > 0 &&
+          blogs.map((blog) => {
+            return <Card store={blog} />;
+          })}
       </div>
     </Layout>
   );
--- React Blog MS/src/Pages/Blog/SingleBlog.jsx
+++ React Blog MS/src/Pages/Blog/SingleBlog.jsx
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../Components/Layout/Layout";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import { baseUrl } from "../../config";
 
 const SingleBlog = () => {
+  const { id } = useParams();
+  const [blog, setBlog] = useState({});
+
+  const navigate = useNavigate();
+  const deleteBlog = async () => {
+    try {
+      const response = await axios.delete(`${baseUrl}/blog/${id}`, {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+      });
+      if (response.status === 200) {
+        navigate("/");
+      } else {
+        alert("Sorry!! Try Again");
+      }
+    } catch (error) {
+      alert(error.response.data.message);
+    }
+  };
+
+  const fetchBlog = async () => {
+    const response = await axios.get(`${baseUrl}/blog/${id}`);
+    if (response.status === 200) {
+      setBlog(response.data.data);
+    }
+  };
+
+  useEffect(() => {
+    fetchBlog();
+  }, []);
+
   return (
     <Layout>
       <div className="">
@@ -13,7 +47,7 @@ const SingleBlog = () => {
                 <div className="h-[460px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
                   <img
                     className="w-full h-full object-cover"
-                    src="https://cdn.pixabay.com/photo/2020/05/22/17/53/mockup-5206355_960_720.jpg"
+                    src={blog?.imageUrl}
                     alt="Product Image"
                   />
                 </div>
@@ -27,7 +61,10 @@ const SingleBlog = () => {
                     </Link>
                   </div>
                   <div className="w-1/2 px-2">
-                    <button className="w-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600">
+                    <button
+                      className="w-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600 "
+                      onClick={deleteBlog}
+                    >
                       Delete
                     </button>
                   </div>
@@ -35,17 +72,15 @@ const SingleBlog = () => {
               </div>
               <div className="md:flex-1 px-4">
                 <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-2">
-                  Blog Title
+                  {blog?.title}
                 </h2>
                 <p className="text-gray-600 dark:text-gray-300 text-sm mb-4">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                  sed ante justo. Integer euismod libero id mauris malesuada
-                  tincidunt.
+                  {blog?.subtitle}
                 </p>
                 <div className="flex mb-4">
                   <div className="mr-4">
                     <span className="font-bold text-gray-700 dark:text-gray-300">
-                      Category:
+                      {blog.category}
                     </span>
                     <span className="text-gray-600 dark:text-gray-300">
                       $29.99
@@ -53,10 +88,7 @@ const SingleBlog = () => {
                   </div>
                   <div>
                     <span className="font-bold text-gray-700 dark:text-gray-300">
-                      Publish At:
-                    </span>
-                    <span className="text-gray-600 dark:text-gray-300">
-                      In Stock
+                      Author: {blog?.userId?.username}
                     </span>
                   </div>
                 </div>
@@ -66,14 +98,7 @@ const SingleBlog = () => {
                     Product Description:
                   </span>
                   <p className="text-gray-600 dark:text-gray-300 text-sm mt-2 ">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                    sed ante justo. Integer euismod libero id mauris malesuada
-                    tincidunt. Vivamus commodo nulla ut lorem rhoncus aliquet.
-                    Duis dapibus augue vel ipsum pretium, et venenatis sem
-                    blandit. Quisque ut erat vitae nisi ultrices placerat non
-                    eget velit. Integer ornare mi sed ipsum lacinia, non
-                    sagittis mauris blandit. Morbi fermentum libero vel nisl
-                    suscipit, nec tincidunt mi consectetur.
+                    {blog?.description}
                   </p>
                 </div>
               </div>
